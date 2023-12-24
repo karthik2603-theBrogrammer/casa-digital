21660e9addf979c276f7f38f8346f3fc1e0e0e54
Karthik Namboori <103726023+karthik2603-theBrogrammer@users.noreply.github.com>
2023-12-25 00:07:43 +0530

    minor fixes to web

--- legacy/mpca-ui-web/src/Components/LandingPage.jsx
+++ legacy/mpca-ui-web/src/Components/LandingPage.jsx
@@ -34,33 +34,30 @@ const LandingPage = () => {
             </h1>
 
             <h1 class="mb-2 text-[60px] font-extrabold leading-none tracking-tight text-gray-900 md:text-9xl  dark:text-white">
-              Welcome to casaDigital
+              Welcome to Casa digital!
             </h1>
           </motion.div>
           <motion.div
             {...headContentAnimation}
             className="flex flex-col gap-3  justify-center px-6"
           >
-            <p className="my-1 text-md text-gray-800 md:text-[28px]">
-              casaDigital has been built as our MPCA Semester 4 Project. It makes
-              use of the iOt technology, namely the ESP8266 and Arduino-Uno as
-              the microcontrollers. The Arduino Uno houses(pun intended) the
-              components and transmits the data to the ESP8266 that recieves the
-              data from the home and transmits to the Node Sever. From there its
-              sent to the cloud and with the help of SSR i.e Server Side
-              Rendering, a popular cloud feature, its reflected onto your
-              frontend web user interface you are seeing currently.
+            <p className="my-1 text-md text-gray-800 md:text-[28px] md:p-6">
+
+              Casa Digital: Real-time IoT data processing for smart homes. 
+              Utilizes <span className="font-bold text-warning-content">MQTT, Kafka, and TimescaleDB</span>. 
+              Connects sensors, streams data, and stores in TimescaleDB. Node.js integration, plus 
+              CLI tool for analysis.
             </p>
 
-          
+
           </motion.div>
           <motion.div {...headContentAnimation} className="my-9 mx-4">
-            <span
+            {/* <span
               className="bg-purple-400 rounded-lg text-white p-3 text-1xl "
               onClick={() => (state.page = "dashboard")}
             >
               Proceed
-            </span>
+            </span> */}
             <span
               className="bg-purple-400 rounded-lg text-white p-3 text-1xl mx-6 "
               onClick={() => (state.page = "admin")}
